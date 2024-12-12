c9cc0a8ebfd40efcceb6399686862f0363aa7084
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2024-12-12 21:42:12 +0300

    Update Program.cs
    Create classs Driver

--- Park_Bus/Program.cs
+++ Park_Bus/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿string A = "pr";
+string B = "pr";
+string C = "pr";
+string D = "100000";
+
+Driver driver = new Driver(A, B, C, D);
+var i = driver.Name;
+Console.WriteLine(i);
+
+public class Driver
+{
+    private string surname, name, patronymic;
+    private string numberDL;
+    private string id_driver;
+
+    public string Surname{get { return surname; }set { surname = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public string Patronymic { get {  return patronymic; } set {  patronymic = value; } }
+    public string NumberDL {  get { return numberDL; } set { if (value.Length == 6 && int.TryParse(value, out int num) && value[0] != '0') { numberDL = value; } else { Console.WriteLine("Номер водительского удостоверения должен быть шестизначным! и не должен начинаться с нуля"); } } }
+    public string IdDriver { get { return id_driver; } set { id_driver = value; } }
+
+    public Driver(string a, string b, string c, string d)
+    {
+        surname = a;
+        name = b;
+        patronymic = c;
+        numberDL = d;
+    }
+}
+
