7d62cd4adc75e7d17922b02a8cfb5925a8c8b918
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2024-12-13 23:18:03 +0300

    Переделана проверка на вод.удостоверение

--- Park_Bus/Claas_Driver.cs
+++ Park_Bus/Claas_Driver.cs
@@ -7,7 +7,15 @@
     public string Surname{get { return surname; }set { surname = value; } }
     public string Name { get { return name; } set { name = value; } }
     public string Patronymic { get {  return patronymic; } set {  patronymic = value; } }
-    public string NumberDL {  get { return numberDL; } set { if (value.Length == 6 && int.TryParse(value, out int num) && value[0] != '0') { numberDL = value; } else { Console.WriteLine("Номер водительского удостоверения должен быть шестизначным! и не должен начинаться с нуля"); } } }
+    public string NumberDL {  get { return numberDL; } set {
+            if (value.Length != 6)
+            {
+                Console.WriteLine("Номер водительского удостоверения должен быть шестизначным!");
+            }
+            else if (value[0] == '0') 
+            { Console.WriteLine("Номер водительского удостоверения не должен начинаться с 0!");
+            }
+            else { numberDL = value; } } }
    // public string IdDriver { get { return id_driver; } set { id_driver = value; } }
 
     public Driver(string a, string b, string c, string d)
@@ -15,7 +23,12 @@
         surname = a;
         name = b;
         patronymic = c;
-        numberDL = d;
+        this.NumberDL = d;
+    }
+
+    public void driver_info()
+    {
+        Console.WriteLine(this.NumberDL);
     }
 }
 
