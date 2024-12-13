58117f7f06d25e2d72e49c0b5838d3e7bc448155
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2024-12-13 23:18:33 +0300

    Сделано назначение водителя на автобус

--- Park_Bus/Class_Bus.cs
+++ Park_Bus/Class_Bus.cs
@@ -1,6 +1,31 @@
 public class Bus
 {
-    private string Bus_Name;
+    private string bus_name;
     private int number_of_seats;
+    private string bus_number;
+    public Driver? drivers { get; set; }
+    //public Company company { get; set; }
+    public string Bus_Name { get { return bus_name; }}
+    public int Number_of_Seats { get { return number_of_seats; }}
+    public string Bus_Number { get { return bus_number; }}
+    public Bus(string name, int nos, string bn)
+    {
+        bus_name = name;
+        number_of_seats = nos;
+        bus_number = bn;
+    }
 
+    public void assign_a_driver(Driver driver, Company company)
+    {
+        if (company.Search_Driver(driver))
+        {
+            drivers = driver;
+        }
+        else { Console.WriteLine("Такого водителя нет в базе!"); }
+    }
+
+    public void remove_the_driver(Driver driver)
+    {
+        drivers = null;
+    }
 }
