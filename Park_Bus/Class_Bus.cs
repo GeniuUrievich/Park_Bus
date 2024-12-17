65c768fcd188cb78daa5862989d7a212cc789824
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2024-12-17 20:35:46 +0300

    Добавлена функция Информации об автобусе

--- Park_Bus/Class_Bus.cs
+++ Park_Bus/Class_Bus.cs
@@ -5,9 +5,9 @@ public class Bus
     private string bus_number;
     public Driver? drivers { get; set; }
     //public Company company { get; set; }
-    public string Bus_Name { get { return bus_name; }}
-    public int Number_of_Seats { get { return number_of_seats; }}
-    public string Bus_Number { get { return bus_number; }}
+    public string Bus_Name { get { return bus_name; } }
+    public int Number_of_Seats { get { return number_of_seats; } }
+    public string Bus_Number { get { return bus_number; } }
     public Bus(string name, int nos, string bn)
     {
         bus_name = name;
@@ -27,5 +27,17 @@ public class Bus
     public void remove_the_driver(Driver driver)
     {
         drivers = null;
+    }
+
+    public void info_bus()
+    {
+        if (drivers != null)
+        {
+            Console.WriteLine($"јвтобус: {this.Bus_Name}, количество мест дл€ сидени€: {this.Number_of_Seats}, водитель: {this.drivers.Surname} {this.drivers.Name}");
+        }
+        else
+        {
+            Console.WriteLine($"јвтобус: {this.Bus_Name}, количество мест дл€ сидени€: {this.Number_of_Seats}, водитель: не назначен");
+        }
     }
 }
